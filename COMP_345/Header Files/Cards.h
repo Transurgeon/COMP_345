9051775abd1ec48682cadfc863d020788826d164
Nader Nasr <na_nas@live.concordia.ca>
2022-02-09 13:47:21 -0500

    Cards.h Additions

--- COMP_345/Header Files/Cards.h
+++ COMP_345/Header Files/Cards.h
@@ -7,5 +7,33 @@ using namespace std;
 
 class Card {
 	Card();
+	void play();
+	Card* bomb;
+	Card* reinforcement;
+	Card* blockade;
+	Card* airlift;
+	Card* diplomacy;
 };
+
+class Deck
+{
+public:
+	Deck();
+	~Deck();
+
+	void draw();
+
+};
+
+class Hand
+{
+public:
+	Hand();
+	~Hand();
+
+	[] Card* warzoneCards;
+
+};
+
 #endif
+
