40858feee4b39e0a349e6f040ba0d072ad965c28
KirkDesktop <patoucheask@gmail.com>
2022-02-10 16:24:22 -0500

    added headers to map.cpp

--- COMP_345/Source Files/Map.cpp
+++ COMP_345/Source Files/Map.cpp
@@ -1,6 +1,58 @@
 #include "../Header Files/Map.h"
+#include <iostream>
+#include "Map.h"
+using namespace std;
 
-Territory::Territory() {
-
+Map::Map() 
+{
 
 }
+
+Map::bool validate() 
+{
+
+}
+
+Map::Map(Territory* [], Continent* [], Borders* [])
+{
+
+}
+
+Map::~Map()
+{
+
+}
+
+Territory::Territory() 
+{
+
+}
+
+Territory::Territory(string& title, int continent, int countryNum)
+{
+
+}
+
+Territory::~Territory()
+{
+
+}
+
+Continent::Continent()
+{
+
+}
+
+Continent::Continent(Territory* [])
+{
+
+}
+
+Continent::~Continent()
+{
+
+}
+
+
+
+
