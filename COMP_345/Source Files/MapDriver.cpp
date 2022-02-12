658e6cd08f25cdb605a267fe501763c2a2da567e
Transurgeon <petertakayuki@gmail.com>
2022-02-12 00:48:58 -0500

    added commented main and #include header file for all drivers

--- COMP_345/Source Files/MapDriver.cpp
+++ COMP_345/Source Files/MapDriver.cpp
@@ -1,7 +1,6 @@
-// COMP_345.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "../Header Files/Map.h"
+#include <iostream>
 
-//#include <iostream>
 //void showMenu() {
 //    std::cout << "Yo does this work";
 //}
@@ -16,18 +15,3 @@
 //
 //    system("pause>0");
 //}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-//Test
-
-//changes in A1
